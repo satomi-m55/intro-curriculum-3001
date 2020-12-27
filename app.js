325b22d200e9a5a94fbaa7de230bf3a4c02b1f6d
satomi-m55 <satosuke52526@live.jp>
2020-12-28 01:00:40 +0900

    階乗計算プログラム

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ function factorial(n) {
 	return result;
 }
 const assert = require('assert');
-assert.equal(factorial(1), 1, `1の階乗は1ですが、実際は${factorial(1) }でした`);
+assert.equal(factorial(1), 1, `1の階乗は1です、実際は${factorial(1) }でした`);
 assert.equal(factorial(2), 2, `2の階乗は2ですが、実際は${factorial(2) }でした`);
 assert.equal(factorial(3), 6, `3の階乗は6ですが、実際は${factorial(3) }でした`);
 assert.equal(factorial(10), 3628800, `10の階乗は3628800ですが、実際は${factorial(10) }でした`);
